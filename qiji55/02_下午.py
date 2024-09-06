9a81b108e09ee4d4e9da38201372996d16404b78
liangzhao <liang_zhao02@163.com>
2024-09-06 15:20:24 +0800

    新增与修改

--- qiji55/02_下午.py
+++ qiji55/02_下午.py
@@ -1,10 +1,20 @@
-age = int(input("请输入你的年龄："))
-if age >= 18:
-    print("运行进入网吧")
-    money = int(input("请输入现在余额："))
-    if money >= 10:
-        print("可以上网")
+# age = int(input("请输入你的年龄："))
+# if age >= 18:
+#     print("运行进入网吧")
+#     money = int(input("请输入现在余额："))
+#     if money >= 10:
+#         print("可以上网")
+#     else:
+#         print("不允许上网")
+# else:
+#     print("不允许进入网吧")
+money = int(input("请输入你的公交卡余额："))
+if money > 2:
+    print("上车了")
+    seat = int(input("车上是否还有剩余座位：")) # 1 有座位 0 没座位
+    if seat == 1:
+        print("坐下了")
     else:
-        print("不允许上网")
+        print("站稳扶好")
 else:
-    print("不允许进入网吧")
+    print("没钱别做公交车")
