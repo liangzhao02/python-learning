dd30fc10391d315ce9c4c9c41a651439e6eb7b57
liangzhao <liang_zhao02@163.com>
2024-09-07 11:11:03 +0800

    新增与修改

--- qiji55/h_2.py
+++ qiji55/h_2.py
@@ -2,8 +2,14 @@
 
 i = 1
 sum = 0
-while i <= 1000:
+while i < 1000:
     if i % 7 != 0:
         sum += i
     i += 1
 print(sum)
+
+# sum_value = 0
+# for i in range(1000):
+#     if i % 7!= 0:
+#         sum_value += i
+# print(sum_value)
