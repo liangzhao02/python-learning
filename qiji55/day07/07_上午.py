e29cf59e29cf678e727579524d916effe73db81a
liangzhao <liang_zhao02@163.com>
2024-09-14 10:36:33 +0800

    Merge remote-tracking branch 'python_qiji55/master'

--- qiji55/day07/07_上午.py
+++ qiji55/day07/07_上午.py
@@ -21,6 +21,54 @@
 # def demo4(a,b):
 #     return a + b
 
-demo4 = lambda a,b:a + b
-result = demo4(1,2)
-print(result)
+#
+# demo4 = lambda a,b:a + b
+# result = demo4(1,2)
+# print(result)
+
+# 需求:以列表的形式输出1-100之间的所有数
+# 方式1
+# li = []
+# for i in range(1,101):
+#     li.append(i)
+
+# 方式2
+# li = [i for i in range(1,101)]
+# print(li)
+
+# 需求:以列表的形式输出1-100之间的所有的偶数
+# li = [i for i in range(2,101,2)]
+# li = [i for i in range(1,101) if i % 2 == 0]
+# print(li)
+# 需求:以列表的形式输出以下两字名字的各种组合
+# a = ['赵','王','孙','李']
+# b = ['一','二','三','四']
+# li = [x + y for x in a for y in b]
+# print(li)
+
+# map:是一个函数,可以将列表按照指定规则输出
+# 输出列表['1','2','3','4']
+li =[1,2,3,4]
+# 方式1
+# li1 = []
+# for i in li:
+#     li1.append(str(i))
+# print(li1)
+
+# 方式2
+# li1 =[str(i) for i in li]
+# print(li1)
+
+def demo(x):
+    return str(x)
+
+res = list(map(demo,li))
+print(res)
+
+
+
+
+
+
+
+
