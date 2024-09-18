78cedd0dc3526661625fb7662fa49e6dc83fe074
liangzhao <liang_zhao02@163.com>
2024-09-18 14:54:28 +0800

    update

--- qiji55/day09/practise.py
+++ qiji55/day09/practise.py
@@ -1,35 +1,3 @@
-class Person(object):
-
-    def __init__(self, name, sex, age, country):
-        self.name = name
-        self.sex = sex
-        self.age = age
-        self.country = country
-
-    def eat(self):
-        print('%s要吃饭' % self.name)
-
-    def sleep(self):
-        print('%s要睡觉' % self.name)
-
-    def work(self):
-        print('%s要工作' % self.name)
-
-    def __str__(self):
-        attrs = vars(self) #返回字典
-        attr_list = [ "%s: %s" % (key, value) for key, value in attrs.items()]
-        return '\n'.join(attr_list)
-
-p = Person('张三','男',18,'China')
-p.eat()
-p.sleep()
-p.work()
-# print(p.name)
-# print(p.sex)
-# print(p.age)
-# print(p.country)
-print(p)
-
 # class Person(object):
 #
 #     def __init__(self, name, sex, age, country):
@@ -47,31 +15,69 @@ print(p)
 #     def work(self):
 #         print('%s要工作' % self.name)
 #
-# class Student(Person):
-#     def __init__(self, name, sex, age, country, school_name, no):
-#         super().__init__(name, sex, age, country)
-#         self.school_name = school_name
-#         self.no = no
-#
-#     def eat(self):
-#         print('%s要吃饭' % self.name)
-#
-#     def sleep(self):
-#         print('%s要睡觉' % self.name)
-#
-#     def work(self):
-#         print('%s要学习' % self.name)
-#
+#     # def __str__(self):
+#     #     attrs = vars(self) #返回字典
+#     #     attr_list = [ "%s: %s" % (key, value) for key, value in attrs.items()]
+#     #     return '\n'.join(attr_list)
 #     def __str__(self):
-#         attrs = vars(self) # 返回字典
-#         attr_list = [ "%s: %s" % (key, value) for key, value in attrs.items()]
-#         return '\n'.join(attr_list)
+#         return '姓名为%s,性别为%s,年龄为%s,国籍为%s' % (self.name,self.sex,self.age,self.country)
+
+# p = Person('张三','男',18,'China')
+# p.eat()
+# p.sleep()
+# p.work()
+# print(p.name)
+# print(p.sex)
+# print(p.age)
+# print(p.country)
+# print(p)
+
+class Person(object):
+
+    def __init__(self, name, sex, age, country):
+        self.name = name
+        self.sex = sex
+        self.age = age
+        self.country = country
+
+    def eat(self):
+        print('%s要吃饭' % self.name)
+
+    def sleep(self):
+        print('%s要睡觉' % self.name)
+
+    def work(self):
+        print('%s要工作' % self.name)
+
+class Student(Person):
+    def __init__(self, name, sex, age, country, school_name, no):
+        super().__init__(name, sex, age, country)
+        self.school_name = school_name
+        self.no = no
+
+    def eat(self):
+        print('%s要吃饭' % self.name)
+
+    def sleep(self):
+        print('%s要睡觉' % self.name)
+
+    def work(self):
+        print('%s要学习' % self.name)
+
+    # def __str__(self):
+    #     attrs = vars(self) # 返回字典
+    #     attr_list = [ "%s: %s" % (key, value) for key, value in attrs.items()]
+    #     return '\n'.join(attr_list)
+
+    def __str__(self):
+        return '姓名为%s,性别为%s,年龄为%s,国籍为%s,学校是%s,学号是%s' % (self.name, self.sex, self.age, self.country,self.school_name,self.no)
+
 # # p = Student('张三','男',18,'China','qijiketang','001')
-# student = Student("张三", "男", 18, "中国", "奇技课堂", "001")
-# student.eat()
-# student.sleep()
-# student.work()
-# print(student)
+student = Student("张三", "男", 18, "中国", "奇技课堂", "001")
+student.eat()
+student.sleep()
+student.work()
+print(student)
 
 
 
