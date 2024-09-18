3d30f369a33ee49b27aa8429371c94b5708acd91
liangzhao <liang_zhao02@163.com>
2024-09-18 15:03:22 +0800

    update

--- qiji55/day09/practise.py
+++ qiji55/day09/practise.py
@@ -51,7 +51,7 @@ class Person(object):
 
 class Student(Person):
     def __init__(self, name, sex, age, country, school_name, no):
-        super().__init__(name, sex, age, country)
+        super(Student ,self).__init__(name, sex, age, country)
         self.school_name = school_name
         self.no = no
 
@@ -64,13 +64,15 @@ class Student(Person):
     def work(self):
         print('%s要学习' % self.name)
 
-    # def __str__(self):
-    #     attrs = vars(self) # 返回字典
-    #     attr_list = [ "%s: %s" % (key, value) for key, value in attrs.items()]
-    #     return '\n'.join(attr_list)
-
     def __str__(self):
-        return '姓名为%s,性别为%s,年龄为%s,国籍为%s,学校是%s,学号是%s' % (self.name, self.sex, self.age, self.country,self.school_name,self.no)
+        attrs = vars(self) # 返回字典
+        attr_list = [ "%s: %s" % (key, value) for key, value in attrs.items()]
+        return '\n'.join(attr_list)
+    # def __str__(self):
+    #     res = super(Student ,self).__str__()
+    #     return '%s,校名%s,学号为%s' % (res,self.school_name,self.no)
+    # def __str__(self):
+    #     return '姓名为%s,性别为%s,年龄为%s,国籍为%s,学校是%s,学号是%s' % (self.name, self.sex, self.age, self.country,self.school_name,self.no)
 
 # # p = Student('张三','男',18,'China','qijiketang','001')
 student = Student("张三", "男", 18, "中国", "奇技课堂", "001")
